83b579816345a6b9e841edb9f8d9cb4fdb50177a
Pablo Torres <torres.m.pablo@gmail.com>
2024-10-01 13:00:58 -0300

    Se mejora UI del NavBAr

--- surferMarketProject/frontend/src/components/SurferHeader.jsx
+++ surferMarketProject/frontend/src/components/SurferHeader.jsx
@@ -1,10 +1,35 @@
+import { useState } from 'react'
 import tableSurf from '../assets/icons/surfing-board.png'
 
 export const SurferHeader = () => {
+
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value); // Update the state with the user's input
+  };
+
   return (
     <nav className='navbar'>
-      <img src={ tableSurf } alt='Logo' className='navbar-logo'></img>
-      <h1 className='navbar-title'>surf market</h1>
+      <div className='navbar-left'>
+        <img src={ tableSurf } alt='Logo' className='navbar-logo'></img>
+        <h1 className='navbar-title'>surf market</h1>        
+      </div>
+
+      <input 
+        type="text" 
+        placeholder="Look up something..." 
+        value={searchTerm} 
+        onChange={handleInputChange} 
+        className="search-input"
+      >
+      </input>
+
+      <div className='navbar-right'>
+        <img src={ tableSurf } alt='Logo' className='navbar-logo'></img>
+        <h1 className='navbar-title'>surf market</h1>        
+      </div>
+
     </nav>
   )
 }
