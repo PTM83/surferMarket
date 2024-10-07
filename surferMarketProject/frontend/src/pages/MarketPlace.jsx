31adb8efb54092fafcbfba65ec0e444af42dc2f0
Pablo Torres <torres.m.pablo@gmail.com>
2024-10-06 23:51:12 -0300

    Se elimina la carpte Context - Se cambia el Provider - Se debe trabajar el Componente Product Form

--- surferMarketProject/frontend/src/pages/MarketPlace.jsx
+++ surferMarketProject/frontend/src/pages/MarketPlace.jsx
@@ -3,20 +3,12 @@ import { SurferCardProduct } from '../components/SurferCardProduct';
 import { SurferFooter } from '../components/SurferFooter';
 import { SurferHeader } from '../components/SurferHeader';
 import NavigationButtons from '../components/NavigationButtons';
-import { useContext } from 'react';
-import { ProductContext } from '../Context/ProductProvider'; 
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom'; 
 import boton_vender from '../assets/icons/boton_vender.png';  
 
 export const MarketPlace = () => {
-  const { addToCart } = useContext(ProductContext); 
-  const [products, setProducts] = useState([]);
-  const navigate = useNavigate(); 
 
-  const handleAddProduct = (newProduct) => {
-    setProducts((prevProducts) => [...prevProducts, newProduct]);
-  };
+  const navigate = useNavigate(); 
 
   const handleNavigateToForm = () => {
     navigate('/add-product'); 
@@ -36,7 +28,7 @@ export const MarketPlace = () => {
         />
       </div>
 
-      <SurferCardProduct products={products} addToCart={addToCart} /> 
+      <SurferCardProduct /> 
 
       <SurferFooter />
     </>
