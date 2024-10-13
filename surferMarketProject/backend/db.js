f8e49796fa1e5731d28c1153b6841b14c7c95885
Pablo Torres <torres.m.pablo@gmail.com>
2024-10-13 20:35:41 -0300

    Se crea el archivo example.env que entrega información de cómo conectar la base de datos

--- surferMarketProject/backend/db.js
+++ surferMarketProject/backend/db.js
@@ -4,7 +4,7 @@ require('dotenv').config() // Cargar las variables del entorno
 // Conectar con la base de datos de PostgeSQL
 
 const pool = new Pool({
-  user: process.env.USER,
+  user: process.env.USER_DB,
   host: process.env.HOST,
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
