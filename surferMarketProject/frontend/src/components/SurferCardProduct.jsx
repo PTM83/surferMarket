f0bcce109e83a16948d535d008a2c10ed1a5dfea
Nicolás Soza <sozawer@hotmail.com>
2024-10-26 21:42:32 -0300

    update frontend deploy 6

--- surferMarketProject/frontend/src/components/SurferCardProduct.jsx
+++ surferMarketProject/frontend/src/components/SurferCardProduct.jsx
@@ -1,6 +1,6 @@
 
 import { useProducts } from '../hooks/useProducts';
-import { useCart } from '../hooks/cartContext';
+import { useCart } from './cartContext';
 
 export const SurferCardProduct = ( ) => { 
   const { products, loading, error } = useProducts();
