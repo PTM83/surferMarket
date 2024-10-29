ff428a0e3ccb97bc4b96f6eaee7d8fdb85dde647
Nicolás Soza <sozawer@hotmail.com>
2024-10-28 23:00:06 -0300

    cambio const url front

--- surferMarketProject/frontend/src/service/apiService.js
+++ surferMarketProject/frontend/src/service/apiService.js
@@ -4,7 +4,7 @@ export async function fetchData(endPoint, options = {}) {
   // const url = '../public/data/surferMarketData.json'
   // Descarga productos
 
-  const url = 'https://surfermarketbackend.onrender.com' + endPoint
+  const url = 'http://localhost:3000' + endPoint
   // const url = 'http://localhost:3000' + endPoint
 
   const response = await fetch(url, options);
