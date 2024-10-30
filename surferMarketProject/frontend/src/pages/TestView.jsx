c224b2827a94f46782f7434fd73450c8f84bb802
Nicolás Soza <sozawer@hotmail.com>
2024-10-30 20:06:32 -0300

    update front de pagina producto

--- surferMarketProject/frontend/src/pages/TestView.jsx
+++ surferMarketProject/frontend/src/pages/TestView.jsx
@@ -1,17 +1,30 @@
-
-import { SurferHeader } from '../components/SurferHeader'
+import { SurferHeader } from '../components/SurferHeader' 
 import { SurferFooter } from '../components/SurferFooter'
-import  NavigationButtons  from '../components/NavigationButtons'
+import NavigationButtons from '../components/NavigationButtons'
 
 export const TestView = () => {
   return (
     <>
-      <SurferHeader/>
+      <SurferHeader />
       <NavigationButtons />
       <section className='content-container'>
-        <h1>
-          TestView
-        </h1>
+        <h1>TestView</h1>
+        {/* Formulario para agregar productos */}
+        <form className="product-form">
+          <div className="form-group">
+            <label htmlFor="productName">Product Name:</label>
+            <input type="text" id="productName" name="productName" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="productPrice">Price:</label>
+            <input type="number" id="productPrice" name="productPrice" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="productDescription">Description:</label>
+            <textarea id="productDescription" name="productDescription" required />
+          </div>
+          <button type="submit" className="submit-btn">Add Product</button>
+        </form>
       </section>
       <SurferFooter />
     </>
