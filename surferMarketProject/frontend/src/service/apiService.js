6b0d218f00b6a944651487a3f0afdedef54f7f58
Pablo Torres <torres.m.pablo@gmail.com>
2024-10-29 22:55:27 -0300

    probando

--- surferMarketProject/frontend/src/service/apiService.js
+++ surferMarketProject/frontend/src/service/apiService.js
@@ -4,7 +4,6 @@ export async function fetchData(endPoint, options = {}) {
   // const url = '../public/data/surferMarketData.json'
   // Descarga productos
 
-  // const url = 'https://surfermarketbackend.onrender.com' + endPoint
   // const url = 'http://localhost:3000' + endPoint
   const url = 'https://backendmarket-s9dp.onrender.com' + endPoint
 
