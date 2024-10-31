2a1c7496131c8ccdc743035640d33909726030c6
Nicolás Soza <sozawer@hotmail.com>
2024-10-31 02:35:09 -0300

    update carrito de compras

--- surferMarketProject/frontend/src/components/ProductForm.jsx
+++ surferMarketProject/frontend/src/components/ProductForm.jsx
@@ -3,7 +3,7 @@ import { SurferHeader } from '../components/SurferHeader';
 import NavigationButtons from '../components/NavigationButtons';
 import '../App.css'; // Asegúrate de importar tu archivo de estilos
 
-const ProductForm = ({ onAddProduct }) => {
+const ProductForm = ({ onAddProduct, carrito = [], sumaTotal = 0, formatPrice, incrementar, decrementar, handleVolverClick }) => {
   const [productName, setProductName] = useState('');
   const [productPrice, setProductPrice] = useState('');
   const [productDescription, setProductDescription] = useState('');
@@ -48,56 +48,40 @@ const ProductForm = ({ onAddProduct }) => {
       <SurferHeader />
       <NavigationButtons />
 
-      <div className="form-container">
-        <form onSubmit={handleSubmit} className="product-form">
-          <h2>Agregar Producto</h2>
+      <nav className="navbar navbar-light bg-info" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <span className="navbar-brand mb-0 h1 text-white" style={{ marginLeft: '80px' }}>🍕 Pizzeria Mamma Mia!</span>
+        <div style={{ display: 'flex', alignItems: 'center', marginRight: '80px' }}>
+          <p className="volver-btn" onClick={handleVolverClick} style={{ cursor: 'pointer', margin: 0, padding: 2 }}>Inicio</p>
+        </div>
+      </nav>
 
-          <label>
-            Nombre del Producto:
-            <input
-              type="text"
-              value={productName}
-              onChange={(e) => setProductName(e.target.value)}
-              placeholder="Ingresa el nombre del producto"
-              required
-            />
-          </label>
-
-          <label>
-            Precio del Producto:
-            <input
-              type="number"
-              value={productPrice}
-              onChange={(e) => setProductPrice(e.target.value)}
-              placeholder="Ingresa el precio del producto"
-              required
-            />
-          </label>
-
-          <label>
-            Descripción del Producto:
-            <textarea
-              value={productDescription}
-              onChange={(e) => setProductDescription(e.target.value)}
-              placeholder="Describe el producto"
-              required
-            />
-          </label>
-
-          <label>
-            Imagen del Producto:
-            <input
-              type="file"
-              accept="image/*" // Aceptar solo imágenes
-              onChange={handleImageChange}
-              required
-            />
-          </label>
-
-          <button type="submit" className="submit-button">
-            Agregar Producto
-          </button>
-        </form>
+      <div className="d-flex flex-column align-items-center">
+        <div style={{ width: '80%', marginTop: '40px' }}>
+          <h5>Detalle del pedido</h5>
+          <hr />
+        </div>
+        {carrito &&
+          carrito.map((producto, index) => (
+            <div key={producto.id} className="d-flex justify-content-between align-items-center" style={{ width: '80%', margin: '20px auto' }}>
+              <div className="d-flex align-items-center">
+                <img src={producto.img} width={100} alt={producto.name} />
+                <h3 style={{ marginLeft: '20px' }}>{producto.name}</h3>
+              </div>
+              <div className="d-flex align-items-center">
+                <span style={{ marginRight: '20px', fontSize: '20px' }}>Precio: ${formatPrice(producto.count * producto.price)}</span>
+                <div className="d-flex align-items-center">
+                  <button className="btn btn-info btn-sm custom-btn mr-2" onClick={() => decrementar(index)}>-</button>
+                  <span className="mx-2" style={{ fontSize: '20px' }}>{producto.count}</span>
+                  <button className="btn btn-danger btn-sm custom-btn ml-2" onClick={() => incrementar(index)}>+</button>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+      <hr style={{ width: '80%', margin: '20px auto' }} />
+      <div style={{ width: '80%', margin: '20px auto', textAlign: 'left' }}>
+        <h1 style={{ fontSize: '5vh' }}>Total: ${formatPrice(sumaTotal)}</h1>
+        <button className="btn btn-success" onClick={() => console.log("Pagar")}>Pagar</button>
       </div>
     </>
   );
