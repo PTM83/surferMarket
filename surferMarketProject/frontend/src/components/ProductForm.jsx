41da2e14072e537d2a0434ce964aec4518e28c2c
Nicolás Soza <sozawer@hotmail.com>
2024-10-31 02:41:50 -0300

    update carrito de compras se volvio al anterior

--- surferMarketProject/frontend/src/components/ProductForm.jsx
+++ surferMarketProject/frontend/src/components/ProductForm.jsx
@@ -1,90 +1,157 @@
-import { useState } from 'react';
-import { SurferHeader } from '../components/SurferHeader';
-import NavigationButtons from '../components/NavigationButtons';
-import '../App.css'; // Asegúrate de importar tu archivo de estilos
+// import { useState } from 'react';
+// import { SurferHeader } from '../components/SurferHeader';
+// import NavigationButtons from '../components/NavigationButtons';
+// import '../App.css' // Asegúrate de importar tu archivo de estilos
 
-const ProductForm = ({ onAddProduct, carrito = [], sumaTotal = 0, formatPrice, incrementar, decrementar, handleVolverClick }) => {
-  const [productName, setProductName] = useState('');
-  const [productPrice, setProductPrice] = useState('');
-  const [productDescription, setProductDescription] = useState('');
-  const [productImage, setProductImage] = useState(null); // Cambiado a null por defecto
+import NavigationButtons from "./NavigationButtons"
+import { SurferFooter } from "./SurferFooter"
+import { SurferHeader } from "./SurferHeader"
+import { useCart } from '../components/cartContext'
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+// const ProductForm = ({ onAddProduct }) => {
+//   const [productName, setProductName] = useState('');
+//   const [productPrice, setProductPrice] = useState('');
+//   const [productDescription, setProductDescription] = useState('');
+//   const [productImage, setProductImage] = useState(null); // Cambiado a null por defecto
 
-    // Validar que todos los campos estén completos
-    if (!productName || !productPrice || !productDescription || !productImage) {
-      alert('Por favor, completa todos los campos.');
-      return;
-    }
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
 
-    // Crear el objeto del producto
-    const newProduct = {
-      name: productName,
-      price: productPrice,
-      description: productDescription,
-      image: URL.createObjectURL(productImage) // Crear una URL para mostrar la imagen
-    };
+//     // Validar que todos los campos estén completos
+//     if (!productName || !productPrice || !productDescription || !productImage) {
+//       alert('Por favor, completa todos los campos.');
+//       return;
+//     }
 
-    // Pasar el producto al componente padre
-    onAddProduct(newProduct);
+//     // Crear el objeto del producto
+//     const newProduct = {
+//       name: productName,
+//       price: productPrice,
+//       description: productDescription,
+//       image: URL.createObjectURL(productImage) // Crear una URL para mostrar la imagen
+//     };
 
-    // Limpiar los campos del formulario
-    setProductName('');
-    setProductPrice('');
-    setProductDescription('');
-    setProductImage(null); // Restablecer a null
-  };
+//     // Pasar el producto al componente padre
+//     onAddProduct(newProduct);
 
-  const handleImageChange = (e) => {
-    const file = e.target.files[0]; // Obtener el primer archivo seleccionado
-    if (file) {
-      setProductImage(file);
-    }
-  };
+//     // Limpiar los campos del formulario
+//     setProductName('');
+//     setProductPrice('');
+//     setProductDescription('');
+//     setProductImage(null); // Restablecer a null
+//   };
+
+//   const handleImageChange = (e) => {
+//     const file = e.target.files[0]; // Obtener el primer archivo seleccionado
+//     if (file) {
+//       setProductImage(file);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <SurferHeader />
+//       <NavigationButtons />
+
+//       <div className="form-container">
+//         <form 
+//           onSubmit={handleSubmit} 
+//           className="product-form">
+          
+//           <h2>Agregar Producto</h2>
+          
+          
+//           <label>
+//             Nombre del Producto:
+//             <input
+//               type="text"
+//               value={productName}
+//               onChange={(e) => setProductName(e.target.value)}
+//               placeholder="Ingresa el nombre del producto"
+//               required
+//             />
+//           </label>
+          
+//           <label>
+//             Precio del Producto:
+//             <input
+//               type="number"
+//               value={productPrice}
+//               onChange={(e) => setProductPrice(e.target.value)}
+//               placeholder="Ingresa el precio del producto"
+//               required
+//             />
+//           </label>
+          
+//           <label>
+//             Descripción del Producto:
+//             <textarea
+//               value={productDescription}
+//               onChange={(e) => setProductDescription(e.target.value)}
+//               placeholder="Describe el producto"
+//               required
+//             />
+//           </label>
+          
+//           <label>
+//             Imagen del Producto:
+//             <input
+//               type="file"
+//               accept="image/*" // Aceptar solo imágenes
+//               onChange={handleImageChange}
+//               required
+//             />
+//           </label>
+          
+//           <button type="submit" className="submit-button">
+//             Agregar Producto
+//           </button>
+//         </form>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default ProductForm;
+
+
+export const ProductForm = () => {
+
+  const { cartItems } = useCart()
 
   return (
     <>
-      <SurferHeader />
-      <NavigationButtons />
+    <SurferHeader />
 
-      <nav className="navbar navbar-light bg-info" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <span className="navbar-brand mb-0 h1 text-white" style={{ marginLeft: '80px' }}>🍕 Pizzeria Mamma Mia!</span>
-        <div style={{ display: 'flex', alignItems: 'center', marginRight: '80px' }}>
-          <p className="volver-btn" onClick={handleVolverClick} style={{ cursor: 'pointer', margin: 0, padding: 2 }}>Inicio</p>
-        </div>
-      </nav>
+    <NavigationButtons />
+      <h1>ProductForm { cartItems.length }</h1>
+    <section className='shopping-cart-container'>
 
-      <div className="d-flex flex-column align-items-center">
-        <div style={{ width: '80%', marginTop: '40px' }}>
-          <h5>Detalle del pedido</h5>
-          <hr />
-        </div>
-        {carrito &&
-          carrito.map((producto, index) => (
-            <div key={producto.id} className="d-flex justify-content-between align-items-center" style={{ width: '80%', margin: '20px auto' }}>
-              <div className="d-flex align-items-center">
-                <img src={producto.img} width={100} alt={producto.name} />
-                <h3 style={{ marginLeft: '20px' }}>{producto.name}</h3>
+      {
+        cartItems.length > 0 
+        ? 
+        
+        <ul className="shopping-cart-list">
+          {cartItems.map((item) => (
+            <li key={item.id} className="shopping-cart-item">
+              <div className="item-info">
+                <img src={item.image_url} alt={item.name} className="item-image" />
+                <span className="item-name"> Nombre del producto: {item.name}</span>
+                <span className="item-name"> Precio del producto: ${parseFloat(item.price).toLocaleString('es-Es')}</span>
+                <button className="remove-button">Eliminar</button>
               </div>
-              <div className="d-flex align-items-center">
-                <span style={{ marginRight: '20px', fontSize: '20px' }}>Precio: ${formatPrice(producto.count * producto.price)}</span>
-                <div className="d-flex align-items-center">
-                  <button className="btn btn-info btn-sm custom-btn mr-2" onClick={() => decrementar(index)}>-</button>
-                  <span className="mx-2" style={{ fontSize: '20px' }}>{producto.count}</span>
-                  <button className="btn btn-danger btn-sm custom-btn ml-2" onClick={() => incrementar(index)}>+</button>
-                </div>
-              </div>
-            </div>
+            </li>
           ))}
-      </div>
-      <hr style={{ width: '80%', margin: '20px auto' }} />
-      <div style={{ width: '80%', margin: '20px auto', textAlign: 'left' }}>
-        <h1 style={{ fontSize: '5vh' }}>Total: ${formatPrice(sumaTotal)}</h1>
-        <button className="btn btn-success" onClick={() => console.log("Pagar")}>Pagar</button>
-      </div>
-    </>
-  );
-};
+        </ul>
 
-export default ProductForm;
+        :
+        <h2>No hay elementos por mostrar</h2>
+      }
+
+    </section>
+
+    <SurferFooter />
+    </>
+  )
+}
+
