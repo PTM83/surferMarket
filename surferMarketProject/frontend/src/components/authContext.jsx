4ea6942ea6ab36cc5b360e1462698969f05f5b15
Nicolás Soza <sozawer@hotmail.com>
2024-11-02 01:31:44 -0300

    update mi perfil front

--- surferMarketProject/frontend/src/components/authContext.jsx
+++ surferMarketProject/frontend/src/components/authContext.jsx
@@ -1,12 +1,28 @@
-import React, { createContext, useContext, useState } from 'react';
+// components/AuthContext.jsx
 
+import { createContext, useContext, useState } from 'react';
+import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+
+// Crear contexto de autenticación
 const AuthContext = createContext();
+
+// Hook personalizado para acceder al contexto de autenticación
+export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
 
-  const login = () => setIsAuthenticated(true);
-  const logout = () => setIsAuthenticated(false);
+  const login = () => {
+    setIsAuthenticated(true);
+    navigate('/userProfile'); // Redirige a la página de perfil después de iniciar sesión
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+    navigate('/login'); // Redirige a la página de inicio de sesión al cerrar sesión
+  };
 
   return (
     <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
@@ -15,4 +31,6 @@ export const AuthProvider = ({ children }) => {
   );
 };
 
-export const useAuth = () => useContext(AuthContext);
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
